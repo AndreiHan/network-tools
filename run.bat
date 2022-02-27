b9dd0ef625f760195ccab71cd9b4f63d99569e27
BuildTools <unconfigured@null.spigotmc.org>
2022-02-27 21:20:44 +0200

    -fixed run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ docker build -t network:latest .
 
 docker run -d --name tools.network network:latest
 
-docker exec -it tools.network /bin/bash
+docker exec -it tools.network python main.py
 
 docker stop tools.network
 
