2f5b2f908af738d4f996fc6258812dd0a0325a6f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-03 00:42:44 +0200

    -hotfix

--- run.bat
+++ run.bat
@@ -4,8 +4,6 @@ docker run -d --name tools.network network:latest
 
 docker exec -it tools.network python main.py
 
-docker stop tools.network
-
 docker rm -f tools.network
 
 docker rmi -f network:latest
